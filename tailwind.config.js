a9d8888a9b600ad654c1de58b01d666fc33bba15
Faojul <faojulazim13@gmail.com>
2024-11-19 22:44:54 +0600

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // content: ["*"],
+tailwind.config = {
   theme: {
     extend: {
       colors: {
@@ -27,3 +25,32 @@ module.exports = {
   },
   plugins: [],
 };
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   // content: ["*"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         Blue: "hsl(246, 80%, 60%)",
+//         lightRedWork: "hsl(15, 100%, 70%)",
+//         softBluePlay: "hsl(195, 74%, 62%)",
+//         lightRedStudy: "hsl(348, 100%, 68%)",
+//         limeGreenExercise: "hsl(145, 58%, 55%)",
+//         violetSocial: "hsl(264, 64%, 52%)",
+//         softOrangeSelfCare: "hsl(43, 84%, 65%)",
+//         veryDarkBlue: "hsl(226, 43%, 10%)",
+//         darkBlue: "hsl(235, 46%, 20%)",
+//         desaturatedBlue: "hsl(235, 45%, 61%)",
+//         paleBlue: "hsl(236, 100%, 87%)",
+//       },
+//       fontFamily: {
+//         Rubik: ["Rubik", "sans-serif"],
+//       },
+//       screens: {
+//         tsf: "375px",
+//         ftf: "425px",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
