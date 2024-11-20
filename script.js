5e3a32a88622c8f14f93fa4d0bc50d24dc59b84e
Faojul <faojulazim13@gmail.com>
2024-11-20 13:49:12 +0600

    updated code

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
-// const buttons = document.querySelectorAll("button");
-// const data_card = document.querySelectorAll("[data-card]");
+const buttons = document.querySelectorAll("button");
+const data_card = document.querySelectorAll("[data-card]");
 
-// //Run getData function if a button is clicked
-// for (let i = 0; i < buttons.length; i++) {
-//   buttons[i].addEventListener("click", (e) => {
-//     buttons[i].classList.add("text-white");
-//     for (let j = 0; j < buttons.length; j++) {
-//       if (i !== j) {
-//         buttons[j].classList.remove("text-white");
-//       }
-//     }
-//     getData(e.target.id);
-//   });
-// }
+//Run getData function if a button is clicked
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", (e) => {
+    buttons[i].classList.add("text-white");
+    for (let j = 0; j < buttons.length; j++) {
+      if (i !== j) {
+        buttons[j].classList.remove("text-white");
+      }
+    }
+    getData(e.target.id);
+  });
+}
 
-// //Give validateAndShowDisplayData function dataJson and also buttonId
-// const getData = async (buttonId) => {
-//   const data = await fetch("data.json");
-//   const dataJson = await data.json();
-//   validateAndShowDisplayData(dataJson, buttonId);
-// };
+//Give validateAndShowDisplayData function dataJson and also buttonId
+const getData = async (buttonId) => {
+  const data = await fetch("data.json");
+  const dataJson = await data.json();
+  validateAndShowDisplayData(dataJson, buttonId);
+};
 
-// //change the elements texts using changeTexts function
-// function changeTexts(getButtonId, buttonId) {
-//   for (let i = 0; i < getButtonId.length; i++) {
-//     data_card[i].querySelector(
-//       "[data-current]"
-//     ).innerText = `${getButtonId[i].timeframes[buttonId].current}hrs`;
-//     data_card[i].querySelector(
-//       "[data-previous]"
-//     ).innerText = `Last Week - ${getButtonId[i].timeframes[buttonId].previous}hrs`;
-//     if (getButtonId[i].timeframes[buttonId].current <= 1) {
-//       data_card[i].querySelector(
-//         "[data-current]"
-//       ).innerText = `${getButtonId[i].timeframes[buttonId].current}hr`;
-//       if (getButtonId[i].timeframes[buttonId].previous <= 1) {
-//         data_card[i].querySelector(
-//           "[data-previous]"
-//         ).innerText = `Last Week - ${getButtonId[i].timeframes[buttonId].previous}hr`;
-//       }
-//     }
-//   }
-// }
+//change the elements texts using changeTexts function
+function changeTexts(getButtonId, buttonId) {
+  for (let i = 0; i < getButtonId.length; i++) {
+    data_card[i].querySelector(
+      "[data-current]"
+    ).innerText = `${getButtonId[i].timeframes[buttonId].current}hrs`;
+    data_card[i].querySelector(
+      "[data-previous]"
+    ).innerText = `Last Week - ${getButtonId[i].timeframes[buttonId].previous}hrs`;
+    if (getButtonId[i].timeframes[buttonId].current <= 1) {
+      data_card[i].querySelector(
+        "[data-current]"
+      ).innerText = `${getButtonId[i].timeframes[buttonId].current}hr`;
+      if (getButtonId[i].timeframes[buttonId].previous <= 1) {
+        data_card[i].querySelector(
+          "[data-previous]"
+        ).innerText = `Last Week - ${getButtonId[i].timeframes[buttonId].previous}hr`;
+      }
+    }
+  }
+}
 
-// //validate the button clicks
-// const validateAndShowDisplayData = (getButtonId, buttonId) => {
-//   if (buttonId === "dailynpm") {
-//     changeTexts(getButtonId, buttonId);
-//   }
-//   if (buttonId === "weekly") {
-//     changeTexts(getButtonId, buttonId);
-//   }
-//   if (buttonId === "monthly") {
-//     changeTexts(getButtonId, buttonId);
-//   }
-// };
-// dd;
+//validate the button clicks
+const validateAndShowDisplayData = (getButtonId, buttonId) => {
+  if (buttonId === "dailynpm") {
+    changeTexts(getButtonId, buttonId);
+  }
+  if (buttonId === "weekly") {
+    changeTexts(getButtonId, buttonId);
+  }
+  if (buttonId === "monthly") {
+    changeTexts(getButtonId, buttonId);
+  }
+};
+dd;
